a6c7f7cf808db573db9aaf7b97c3ed8249a1ab14
Mathieu <mathieu@freedelity.be>
2023-11-14 10:38:05 +0100

    copyright

--- android/src/main/kotlin/be/freedelity/barcode_scanner/util/MrzUtil.kt
+++ android/src/main/kotlin/be/freedelity/barcode_scanner/util/MrzUtil.kt
@@ -1,3 +1,7 @@
+// Copyright 2023 Freedelity. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
 package be.freedelity.barcode_scanner.util
 
 import android.util.Log
