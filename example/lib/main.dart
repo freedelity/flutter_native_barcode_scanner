4e95766409254f29850eeada0b6a31ac8e117030
Mathieu Dussart <mathieu@freedelity.be>
2024-04-10 06:46:46 +0200

    #22 add exemple

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -26,16 +26,13 @@ class MyApp extends StatefulWidget {
   State<MyApp> createState() => _MyAppState();
 }
 
-enum CameraActions {
-  flipCamera,
-  toggleFlashlight,
-  stopScanner,
-  startScanner,
-  setOverlay
-}
+enum CameraActions { flipCamera, toggleFlashlight, stopScanner, startScanner, setOverlay, navigate }
 
 class _MyAppState extends State<MyApp> {
-  bool withOverlay = true;
+  @override
+  void dispose() {
+    super.dispose();
+  }
 
   @override
   void initState() {
@@ -44,144 +41,152 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Scaffold(
-            appBar: AppBar(
-                title: const Text('Scanner plugin example app'),
-                actions: [
-                  PopupMenuButton<CameraActions>(
-                    onSelected: (CameraActions result) {
-                      switch (result) {
-                        case CameraActions.flipCamera:
-                          BarcodeScanner.flipCamera();
-                          break;
-                        case CameraActions.toggleFlashlight:
-                          BarcodeScanner.toggleFlashlight();
-                          break;
-                        case CameraActions.stopScanner:
-                          BarcodeScanner.stopScanner();
-                          break;
-                        case CameraActions.startScanner:
-                          BarcodeScanner.startScanner();
-                          break;
-                        case CameraActions.setOverlay:
-                          setState(() => withOverlay = !withOverlay);
-                          break;
-                      }
-                    },
-                    itemBuilder: (BuildContext context) =>
-                    <PopupMenuEntry<CameraActions>>[
-                      const PopupMenuItem<CameraActions>(
-                        value: CameraActions.flipCamera,
-                        child: Text('Flip camera'),
-                      ),
-                      const PopupMenuItem<CameraActions>(
-                        value: CameraActions.toggleFlashlight,
-                        child: Text('Toggle flashlight'),
-                      ),
-                      const PopupMenuItem<CameraActions>(
-                        value: CameraActions.stopScanner,
-                        child: Text('Stop scanner'),
-                      ),
-                      const PopupMenuItem<CameraActions>(
-                        value: CameraActions.startScanner,
-                        child: Text('Start scanner'),
-                      ),
-                      PopupMenuItem<CameraActions>(
-                        value: CameraActions.setOverlay,
-                        child:
-                        Text('${withOverlay ? 'Remove' : 'Add'} overlay'),
-                      ),
-                    ],
-                  ),
-                ]),
-            body: Builder(builder: (builderContext) {
-              Widget child = BarcodeScannerWidget(
-                scannerType: ScannerType.barcode,
-                onBarcodeDetected: (barcode) async {
-                  await showDialog(
-                      context: builderContext,
-                      builder: (dialogContext) {
-                        return Align(
-                            alignment: Alignment.center,
-                            child: Card(
-                                margin: const EdgeInsets.all(24),
-                                child: Container(
-                                    padding: const EdgeInsets.all(16),
-                                    child: Column(
-                                        mainAxisSize: MainAxisSize.min,
-                                        children: [
-                                          Text('barcode : ${barcode.value}'),
-                                          Text('format : ${barcode.format.name}'),
-                                          ElevatedButton(
-                                              onPressed: () =>
-                                                  Navigator.pop(dialogContext),
-                                              child: const Text('Close dialog'))
-                                        ]))));
-                      });
-                },
-                onTextDetected: (String text) async {
-                  await showDialog(
-                      context: builderContext,
-                      builder: (dialogContext) {
-                        return Align(
-                            alignment: Alignment.center,
-                            child: Card(
-                                margin: const EdgeInsets.all(24),
-                                child: Container(
-                                    padding: const EdgeInsets.all(16),
-                                    child: Column(
-                                        mainAxisSize: MainAxisSize.min,
-                                        children: [
-                                          Text('text : \n$text'),
-                                          ElevatedButton(
-                                              onPressed: () =>
-                                                  Navigator.pop(dialogContext),
-                                              child: const Text('Close dialog'))
-                                        ]))));
-                      });
-                },
-                onError: (dynamic error) {
-                  debugPrint('$error');
-                },
-              );
+    return MaterialApp(home: Builder(builder: (builderContext) {
+      return MyDemoApp();
+    }));
+  }
+}
 
-              if (withOverlay) {
-                return buildWithOverlay(builderContext, child);
+class MyDemoApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _MyDemoAppState();
+}
+
+class _MyDemoAppState extends State<MyDemoApp> {
+  bool withOverlay = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: const Text('Scanner plugin example app'), actions: [
+          PopupMenuButton<CameraActions>(
+            onSelected: (CameraActions result) {
+              switch (result) {
+                case CameraActions.flipCamera:
+                  BarcodeScanner.flipCamera();
+                  break;
+                case CameraActions.toggleFlashlight:
+                  BarcodeScanner.toggleFlashlight();
+                  break;
+                case CameraActions.stopScanner:
+                  BarcodeScanner.stopScanner();
+                  break;
+                case CameraActions.startScanner:
+                  BarcodeScanner.startScanner();
+                  break;
+                case CameraActions.setOverlay:
+                  setState(() => withOverlay = !withOverlay);
+                  break;
+                case CameraActions.navigate:
+                  navigate();
+                  break;
               }
+            },
+            itemBuilder: (BuildContext context) => <PopupMenuEntry<CameraActions>>[
+              const PopupMenuItem<CameraActions>(
+                value: CameraActions.flipCamera,
+                child: Text('Flip camera'),
+              ),
+              const PopupMenuItem<CameraActions>(
+                value: CameraActions.toggleFlashlight,
+                child: Text('Toggle flashlight'),
+              ),
+              const PopupMenuItem<CameraActions>(
+                value: CameraActions.stopScanner,
+                child: Text('Stop scanner'),
+              ),
+              const PopupMenuItem<CameraActions>(
+                value: CameraActions.startScanner,
+                child: Text('Start scanner'),
+              ),
+              PopupMenuItem<CameraActions>(
+                value: CameraActions.setOverlay,
+                child: Text('${withOverlay ? 'Remove' : 'Add'} overlay'),
+              ),
+              const PopupMenuItem<CameraActions>(
+                value: CameraActions.navigate,
+                child: Text('Navigate push'),
+              ),
+            ],
+          ),
+        ]),
+        body: Builder(builder: (builderContext) {
+          Widget child = BarcodeScannerWidget(
+            scannerType: ScannerType.barcode,
+            onBarcodeDetected: (barcode) async {
+              await showDialog(
+                  context: builderContext,
+                  builder: (dialogContext) {
+                    return Align(
+                        alignment: Alignment.center,
+                        child: Card(
+                            margin: const EdgeInsets.all(24),
+                            child: Container(
+                                padding: const EdgeInsets.all(16),
+                                child: Column(mainAxisSize: MainAxisSize.min, children: [Text('barcode : ${barcode.value}'), Text('format : ${barcode.format.name}'), ElevatedButton(onPressed: () => Navigator.pop(dialogContext), child: const Text('Close dialog'))]))));
+                  });
+            },
+            onTextDetected: (String text) async {
+              await showDialog(
+                  context: builderContext,
+                  builder: (dialogContext) {
+                    return Align(
+                        alignment: Alignment.center,
+                        child: Card(
+                            margin: const EdgeInsets.all(24), child: Container(padding: const EdgeInsets.all(16), child: Column(mainAxisSize: MainAxisSize.min, children: [Text('text : \n$text'), ElevatedButton(onPressed: () => Navigator.pop(dialogContext), child: const Text('Close dialog'))]))));
+                  });
+            },
+            onError: (dynamic error) {
+              debugPrint('$error');
+            },
+          );
 
-              return child;
-            })));
+          if (withOverlay) {
+            return buildWithOverlay(builderContext, child);
+          }
+
+          return child;
+        }));
   }
 
   buildWithOverlay(BuildContext builderContext, Widget scannerWidget) {
     return Stack(children: [
       Positioned.fill(child: scannerWidget),
-      Align(
-          alignment: Alignment.center,
-          child: Divider(color: Colors.red[400], thickness: 0.8)),
-      Center(
-          child: Container(
-              margin: const EdgeInsets.symmetric(horizontal: 64),
-              width: double.infinity,
-              height: 200,
-              decoration: BoxDecoration(
-                  border: Border.all(color: Colors.white, width: 2),
-                  borderRadius: BorderRadius.circular(15)))),
+      Align(alignment: Alignment.center, child: Divider(color: Colors.red[400], thickness: 0.8)),
+      Center(child: Container(margin: const EdgeInsets.symmetric(horizontal: 64), width: double.infinity, height: 200, decoration: BoxDecoration(border: Border.all(color: Colors.white, width: 2), borderRadius: BorderRadius.circular(15)))),
       Positioned(
           top: 16,
           right: 16,
           child: ElevatedButton(
-              style: ButtonStyle(
-                  backgroundColor: MaterialStateProperty.all(Colors.purple),
-                  foregroundColor: MaterialStateProperty.all(Colors.white),
-                  shape: MaterialStateProperty.all(const CircleBorder()),
-                  padding: MaterialStateProperty.all(const EdgeInsets.all(8))),
+              style: ButtonStyle(backgroundColor: MaterialStateProperty.all(Colors.purple), foregroundColor: MaterialStateProperty.all(Colors.white), shape: MaterialStateProperty.all(const CircleBorder()), padding: MaterialStateProperty.all(const EdgeInsets.all(8))),
               onPressed: () {
-                ScaffoldMessenger.of(builderContext).showSnackBar(
-                    const SnackBar(content: Text('Icon button pressed')));
+                ScaffoldMessenger.of(builderContext).showSnackBar(const SnackBar(content: Text('Icon button pressed')));
               },
               child: const Icon(Icons.refresh, size: 32))),
     ]);
+  }
+
+  navigate() {
+    Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) {
+      return Scaffold(
+          appBar: AppBar(
+            title: const Text('Navigate test view'),
+          ),
+          body: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const SizedBox(
+                width: double.infinity,
+              ),
+              const Text('Press back button'),
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => MyDemoApp()));
+                  },
+                  child: const Text('Back'))
+            ],
+          ));
+    }));
   }
 }
